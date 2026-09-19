8fb45cba431327fe92366cde0fbfbf4175976fed
r4khul <blackempire9177@gmail.com>
2026-09-19 23:02:46 +0530

    chore(release): prepare v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.escapebranch.pinshot"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
